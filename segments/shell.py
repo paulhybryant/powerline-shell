64ee18035875c95b1334e778af06fd1c71fccf98
paulhybryant <paulhybryant@gmail.com>
2015-12-29 15:08:25 +0800

    .

--- segments/shell.py
+++ segments/shell.py
@@ -1,4 +1,4 @@
-def add_shell_segment():
+def add_shell_segment(powerline):
 
     if powerline.args.shell == 'bash':
         shell_prompt = ' bash'
@@ -8,5 +8,3 @@ def add_shell_segment():
         shell_prompt = ' ?sh'
 
     powerline.append(shell_prompt, Color.SHELL_FG, Color.SHELL_BG)
-
-add_shell_segment()
--- segments/vimode.py
+++ segments/vimode.py
@@ -1,6 +1,4 @@
-def add_vimode_segment():
+def add_vimode_segment(powerline):
 
     vimode_prompt = ' \n${VIMODE}'
     powerline.append(vimode_prompt, Color.NEWLINE_FG, Color.NEWLINE_BG)
-
-add_vimode_segment()
